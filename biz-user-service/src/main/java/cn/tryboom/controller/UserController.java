3c214744f061ee8fe2dc57d53ec2bebefbf5b9fe
biao <18838986375@163.com>
2024-09-18 00:05:52 +0800

    feat: zipkin and logback
    - add zipkin

--- biz-user-service/src/main/java/cn/tryboom/controller/UserController.java
+++ biz-user-service/src/main/java/cn/tryboom/controller/UserController.java
@@ -2,15 +2,20 @@ package cn.tryboom.controller;
 
 import cn.tryboom.biz.api.interfaces.UserService;
 import cn.tryboom.biz.api.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class UserController implements UserService {
 
+    final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Override
     public Boolean saveUser(User user) {
-        System.out.println("user-service save user : " + user.toString());
+        logger.info("user-service save user : {} " , user.toString());
         return Boolean.TRUE;
     }
 }
--- biz-web/src/main/java/cn/tryboom/biz/web/mvc/controller/UserController.java
+++ biz-web/src/main/java/cn/tryboom/biz/web/mvc/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.tryboom.biz.web.mvc.controller;
 
 import cn.tryboom.biz.api.interfaces.UserService;
 import cn.tryboom.biz.api.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +15,21 @@ public class UserController {
 
     private final UserService userService;
 
+    final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping("/save")
     public String saveUser(@RequestBody User user) {
+
+        logger.info("/api/user/save ， user : {}"  , user.toString());
         Boolean saved = userService.saveUser(user);
         if(saved){
-            return "user saved" + user.toString();
+            return "user saved" + user;
         }
+
         return "user not saved";
     }
 }
